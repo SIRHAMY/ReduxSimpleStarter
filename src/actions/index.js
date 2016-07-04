17d5ecea73c0ddfc3a2e34904cd051cbc971220e
Hamilton Greene <hamilton.greene@gmail.com>
2016-07-04 17:31:44 -0500

    s5l57

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,22 @@
+import axios from 'axios';
+
 const API_KEY = "e10ef0f508b96a98a95d06c3c1397d37";
+//const ROOT_URL = "http://api.openweathermap.org/data/2.5/forecast?appid=" + API_KEY;
+
+//This is a template string
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(cityName) {
+
+    const url = `${ROOT_URL}&q=${cityName},us`;
+    const request = axios.get(url);
+
+    
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,13 +1,24 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
 
     constructor(props) {
         super(props);
 
         this.state = { searchTerm: '' };
 
+        // Binding (this) to our component's this.FCN because
+        // these fcns are being passed around as callbacks.
+        // Because callbacks will be executed inside other fcns,
+        // this almost always not be the correct this.
+        // Therefore, we bind this context to it, so that
+        // our declarations containing this executed
+        // as we expect
         this.onInputChange = this.onInputChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
     }
 
     onInputChange(event) {
@@ -18,6 +29,8 @@ export default class SearchBar extends Component {
         event.preventDefault(); 
 
         // Need to fetch weather data 
+        this.props.fetchWeather(this.state.searchTerm);
+        this.setState({ searchTerm: '' });
     }
 
     render() {
@@ -35,3 +48,14 @@ export default class SearchBar extends Component {
         );
     }
 }
+
+//Allows us to access the action creator on our component's
+// props (this.props.fetchWeather())
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+// null as first arg saying that this container doesn't care
+// about app state. Serves as a placeholder because connect
+// takes in the dispatch-to-props-mapper as the second arg
+export default connect(null, mapDispatchToProps)(SearchBar);
