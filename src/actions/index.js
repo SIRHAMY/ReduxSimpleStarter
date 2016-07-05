a38e50662dfd4fce5fb9982780a5b313c372bd7e
Hamilton Greene <hamilton.greene@gmail.com>
2016-07-04 22:18:25 -0500

    s5l58

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,8 +13,6 @@ export function fetchWeather(cityName) {
     const url = `${ROOT_URL}&q=${cityName},us`;
     const request = axios.get(url);
 
-    
-
     return {
         type: FETCH_WEATHER,
         payload: request
