e73f82ba706375627d037c1a41f6ebcfef5aad7e
Hamilton Greene <hamilton.greene@gmail.com>
2016-07-10 22:50:24 -0500

    s6l79

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-    return <div>List of blog posts.</div>;
-};
+class PostsIndex extends Component {
+
+    componentWillMount() {
+        console.log("componentWillMount called");
+    }
+
+    render() {
+        return (
+            <div> List of blog posts </div>
+        );
+    }
+}
+
+export default PostsIndex;
