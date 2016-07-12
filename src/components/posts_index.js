e0ba3af6a720d5cd66f05109acae73240bf60952
Hamilton Greene <hamiltongreene@Hamiltons-MacBook-Pro-2.local>
2016-07-11 20:44:23 -0500

    WorkStuff

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {fetchPosts} from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsIndex extends Component {
 
     componentWillMount() {
-        console.log("componentWillMount called");
+        this.props.fetchPosts();
     }
-
+ 
     render() {
         return (
-            <div> List of blog posts </div>
+            <div> 
+                <div className="text-xs-right">
+                    <Link to="/posts/new" className="btn btn-primary">
+                        Add a post
+                    </Link>
+                </div>
+                List of blog posts 
+            </div>
         );
     }
 }
 
-export default PostsIndex;
+/*
+//Access to this.props.fetchPosts
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ fetchPosts }, dispatch);
+}
+*/
+
+//Access to fetchPosts
+//Setting fetchPosts on props == fetchPosts from import
+export default connect(null, {fetchPosts: fetchPosts})(PostsIndex);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import PostsReducer from './reducer_posts';
+import {reducer as formReducer } from 'redux-form';
+  //Import redux-form, grab reducer property, rename as formReducer
 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  form: formReducer
 });
+//Make sure you use form with formReducer
 
 export default rootReducer;
